19d1fd3068d3275f0df63fd2692f715487a6bd07
Dragan PAUNOVIC <dpaunovi@e1r7p16.42.fr>
2017-02-14 09:52:52 +0100

    fdf finish all

--- main.c
+++ main.c
@@ -6,11 +6,10 @@
 /*   By: dpaunovi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 16:26:24 by dpaunovi          #+#    #+#             */
-/*   Updated: 2017/02/10 18:47:29 by dpaunovi         ###   ########.fr       */
+/*   Updated: 2017/02/14 09:46:50 by dpaunovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
 #include <stdlib.h>
 #include "fdf.h"
 #include "mlx.h"
@@ -22,24 +21,12 @@ int		expose_hook(t_env *e)
 	return (0);
 }
 
-int		key_hook(int keycode, t_env *e)
+void	ft_key(int keycode, t_env *e)
 {
-//	printf("key code : %d\n", keycode);
-	if (keycode == 53)
-		exit(0);
-	if (keycode == 48)
-	{
-		if (e->mode == 1)
-			e->mode = 0;
-		else
-			e->mode = 1;
-	}
 	if (keycode == 69)
 		e->z += 1;
 	if (keycode == 78)
-	{
 		e->z -= 1;
-	}
 	if (keycode == 35)
 		e->depth += 1;
 	if (keycode == 37)
@@ -59,13 +46,21 @@ int		key_hook(int keycode, t_env *e)
 		e->movev -= 100;
 	if (keycode == 125)
 		e->movev += 100;
-	expose_hook(e);
-	return (0);
 }
 
-int		mouse_hook(int button, int x, int y, t_env *e)
+int		key_hook(int keycode, t_env *e)
 {
-//	printf("mouse : %d (%d:%d)\n", button, x, y);
+	if (keycode == 53)
+		exit(0);
+	if (keycode == 48)
+	{
+		if (e->mode == 1)
+			e->mode = 0;
+		else
+			e->mode = 1;
+	}
+	ft_key(keycode, e);
+	expose_hook(e);
 	return (0);
 }
 
@@ -76,7 +71,10 @@ int		main(int argc, char **argv)
 	e.size_x = 1280;
 	e.size_y = 720;
 	if (argc != 2)
+	{
+		ft_putstr("usage: ./fdf [source_file]");
 		return (0);
+	}
 	else
 	{
 		if (lst_tab(file_lst(argv[1]), &e))
@@ -85,7 +83,6 @@ int		main(int argc, char **argv)
 			e.win = mlx_new_window(e.mlx, e.size_x, e.size_y, "FDF");
 			mlx_key_hook(e.win, key_hook, &e);
 			mlx_expose_hook(e.win, expose_hook, &e);
-			mlx_mouse_hook(e.win, mouse_hook, &e);
 			mlx_loop(e.mlx);
 		}
 		else
